4a2fd959a814febf5396a3da780ef292ff6c87fe
Jovino Esteban Guerrero Toledo <CS333972@MinhaTI.com.br>
2021-07-02 12:34:26 -0300

    updating HTML/JS indentation & README.md description

--- README.md
+++ README.md
@@ -1,27 +1,19 @@
-# Desafio
+# [Conversor de Temperatura & Medidor de IMC(BMI)](https://jovinoguerrero.github.io/Temperature-converter-and-BMI-meter/)
 
-## Lista de exercícios para aula de sábado (05/12):
+##### Projeto iniciante feito em HTML/CSS/Javascript e originalmente desenvolvido para uma série de exercícios/desafíos do curso de Programação Full Stack 2020 da [Toti Diversidade](https://totidiversidade.com.br/)
+___
+### Conversor em tempo real de temperatura de Celsius (ºC) a Fahrenheit (ºF) e vice-versa
 
-##### Observação: deve ser desenvolvido algoritmos na linguagem javaScript para cada um dos exercícios abaixo.
+#### Como usar
 
-#### Exercício 1. Desenvolva um programa que recebe uma temperatura em Celsius (ºC) e devolve em Fahrenheit (ºF).
-
-#### Exercício 2. Desenvolva um programa que recebe uma temperatura em Fahrenheit (ºF) e devolve em Celsius (ºC).
-#### Exercício 3. Desenvolva a fórmula para calcular o IMC = peso/altura^2 ou IMC = peso/(altura*altura).
-Desenvolva a fórmula para buscar o resultado correspondente do IMC na Tabela abaixo.
+Só basta com inserir um dado numérico em algum dos campos para ser convertido em tempo real.
+___
+### Medidor de índice de massa corporal(IMC/BMI)
+#### Como usar.
+Inserir peso e altura de uma pessoa tentando de que estes dados sejam os mais próximos da realidade. Logo basta com clicar no botão para obter o resultado numérico junto com uma pequena recomendação.
 * De 0  a 19, Resultado: MUITO MAGRO, Recomendação: DIETA ESPECIAL
 * De 19  a 25, Resultado: NORMAL, Recomendação: MANTER
 * De 25  a 30, Resultado: SOBREPESO, Recomendação: DIETA ESPECIAL
 * De 30  a 40, Resultado: OBESO, Recomendação: ACADEMIA NUTRICIONISTA
 * De 40+, Resultado: OBESIDADE GRAVE, Recomendação: PROCURE UM MÉDICO
-
-#### Exercício 4. Ache o maior número da lista [2, 4, 7, 34, 76, 92].
-#### Exercício 5. Ache o menor número da lista [-1, -5, 56, 34, 91].
 ___
-Material de apoio:  
-
-Fórmula de conversão Celsius para Fahrenheit:  
-(0 °C × 9/5) + 32 = 32 °F  
-
-Fórmula de conversão Fahrenheit para Celsius:  
-(0 °F − 32) × 5/9 = -17,78 °C
--- scripts.js
+++ scripts.js
@@ -5,18 +5,18 @@ let inputF = document.getElementById("inputF");
 
 // Fica atento ao input Celsius
 inputC.addEventListener("input", function(e) {
-    inputF.value = ((this.value*(9/5)) + 32)
+    inputF.value = ((this.value*(9/5)) + 32);
 
     if(this.value == "") {
-        inputF.value = ""
+        inputF.value = "";
     }
 })
 
 // Fica atento ao input Fahrenheit
 inputF.addEventListener("input", function(e) {
-    inputC.value = (this.value - 32) * 5/9
+    inputC.value = (this.value - 32) * 5/9;
     if(this.value == "") {
-        inputC.value = ""
+        inputC.value = "";
     }
 })
 
@@ -31,34 +31,34 @@ let outputRecom = document.getElementById("outputRecom");
 
 // Cancela o submit e a atualização do site
 formIMC.addEventListener("submit", function(e) {
-    e.preventDefault()
+    e.preventDefault();
 })
 
 // Fica atento ao click do botão e mostrar o resultado do calculo do IMC e uma recomendação
 btnIMC.addEventListener("click", function(e) {
-    let IMC = inputPeso.value/(Math.pow(inputAltura.value, 2))
-    IMC = Math.round(IMC * 100) / 100
-    outputRes.value = outputRes.defaultValue + IMC + " Kg/m², "
-    console.log(IMC)
+    let IMC = inputPeso.value/(Math.pow(inputAltura.value, 2));
+    IMC = Math.round(IMC * 100) / 100;
+    outputRes.value = outputRes.defaultValue + IMC + " Kg/m², ";
+    console.log(IMC);
 
     if(IMC >= 0 && IMC < 19){
-        outputRes.value = outputRes.defaultValue + IMC + " Kg/m², " + "MUITO MAGRO"
-        outputRecom.value = outputRecom.defaultValue + "DIETA ESPECIAL"
+        outputRes.value = outputRes.defaultValue + IMC + " Kg/m², " + "MUITO MAGRO";
+        outputRecom.value = outputRecom.defaultValue + "DIETA ESPECIAL";
     } else if(IMC >= 19 && IMC < 25) {
-        outputRes.value = outputRes.defaultValue + IMC + " Kg/m², " + "NORMAL"
-        outputRecom.value = outputRecom.defaultValue + "MANTER"
+        outputRes.value = outputRes.defaultValue + IMC + " Kg/m², " + "NORMAL";
+        outputRecom.value = outputRecom.defaultValue + "MANTER";
     } else if(IMC >= 25 && IMC < 30) {
-        outputRes.value = outputRes.defaultValue + IMC + " Kg/m², " + "SOBREPESO"
-        outputRecom.value = outputRecom.defaultValue + "DIETA ESPECIAL"
+        outputRes.value = outputRes.defaultValue + IMC + " Kg/m², " + "SOBREPESO";
+        outputRecom.value = outputRecom.defaultValue + "DIETA ESPECIAL";
     } else if(IMC >= 30 && IMC < 40) {
-        outputRes.value = outputRes.defaultValue + IMC + " Kg/m², " + "OBESO"
-        outputRecom.value = outputRecom.defaultValue + "ACADEMIA NUTRICIONISTA"
+        outputRes.value = outputRes.defaultValue + IMC + " Kg/m², " + "OBESO";
+        outputRecom.value = outputRecom.defaultValue + "ACADEMIA NUTRICIONISTA";
     } else if(IMC >= 40) {
-        outputRes.value = outputRes.defaultValue + IMC + " Kg/m², " + "OBESIDADE GRAVE"
-        outputRecom.value = outputRecom.defaultValue + "PROCURE UM MÉDICO"
+        outputRes.value = outputRes.defaultValue + IMC + " Kg/m², " + "OBESIDADE GRAVE";
+        outputRecom.value = outputRecom.defaultValue + "PROCURE UM MÉDICO";
     } else {
-        outputRes.value = outputRes.defaultValue + "Cálculo não válido!"
-        outputRecom.value = outputRecom.defaultValue + "Por favor, revise os valores de peso y altura"
+        outputRes.value = outputRes.defaultValue + "Cálculo não válido!";
+        outputRecom.value = outputRecom.defaultValue + "Por favor, revise os valores de peso y altura";
     }
 })
 
@@ -82,4 +82,4 @@ for(let i = 0; i < array2.length; i++){
         menorNum = array2[i];
     }
 }
-console.log("Número menor do array [-1, -5, 56, 34, 91]: "+menorNum)
+console.log("Número menor do array [-1, -5, 56, 34, 91]: "+menorNum);
